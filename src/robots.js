b77f617ac172a91a2e4dbec1ce7c2f164dfa6578
Brandon C <LIGHTMATTR@Brandons-iMac.local>
2020-02-14 22:17:50 -0800

    trying again

--- src/robots.js
+++ src/robots.js
@@ -2,61 +2,61 @@ export const robots = [
   {
     id: 1,
     character: 'Elliot Alderson',
-    image: "https://m.media-amazon.com/images/M/MV5BYzllMjE1MTItZjQ5Mi00NTU2LWFiMTgtMGNkNzlhYzVkZmMzXkEyXkFqcGdeQXVyNjc5Mjg0NjU@._V1_.jpg",
+    image: "./images/rami.jpg",
     actor: 'Rami Malek'
   },
   {
     id: 2,
     character: 'Mr. Robot',
-    image: "https://m.media-amazon.com/images/M/MV5BZDRiZTYwZjEtNmEyYy00NGQxLWE3MTAtNjMxM2NlNDRiYjkzXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
+    image: "./images/christian.jpg",
     actor: 'Christian Slater'
   },
   {
     id: 3,
     character: 'Darlene',
-    image: "https://m.media-amazon.com/images/M/MV5BMTYwNDI3MDA1Ml5BMl5BanBnXkFtZTgwMDYwNzA5OTE@._V1_.jpg",
+    image: "./images/darlene.jpg",
     actor: 'Carly Chaikin'
   },
   {
     id: 4,
     character: 'Tyrell Wellick',
-    image: "https://m.media-amazon.com/images/M/MV5BMTU0MjU1NDk1MF5BMl5BanBnXkFtZTgwNTU1MTExMDI@._V1_.jpg",
+    image: "./images/tyrell.jpg",
     actor: 'Martin Wallström'
   },
   {
     id: 5,
     character: 'Angela Moss',
-    image: "https://m.media-amazon.com/images/M/MV5BMDYxNjcwYTktNGE3Mi00ZmNhLWFiNWYtMzExN2E0YzcyNDA5XkEyXkFqcGdeQXVyNjc5Mjg0NjU@._V1_.jpg",
+    image: "./images/angela.jpg",
     actor: 'Portia Doubleday'
   },
   {
     id: 6,
     character: 'Phillip Price',
-    image: "https://m.media-amazon.com/images/M/MV5BMzg4OTc1MTAxMV5BMl5BanBnXkFtZTgwMTM3ODMyOTE@._V1_.jpg",
+    image: "./images/phillip.jpg",
     actor: 'Michael Cristofer'
   },
   {
     id: 7,
     character: 'Dominique DiPierro',
-    image: "https://m.media-amazon.com/images/M/MV5BMjExNDE0Mzc1Ml5BMl5BanBnXkFtZTgwNjI3ODMyOTE@._V1_.jpg",
+    image: "./images/dominique.jpg",
     actor: 'Grace Gummer'
   },
   {
     id: 8,
     character: 'Whiterose',
-    image: "https://m.media-amazon.com/images/M/MV5BMDAwM2RjZWMtMjcwNy00MGNhLWE4NzUtYTI5MDc5NDdmYmRhXkEyXkFqcGdeQXVyNTQ0NjQzNTE@._V1_.jpg",
+    image: "./images/whiterose.jpg",
     actor: 'BD Wong'
   },
   {
     id: 9,
     character: 'Joanna Wellick',
-    image: "https://m.media-amazon.com/images/M/MV5BMTY3MDQwMzIyMV5BMl5BanBnXkFtZTgwMzM3ODMyOTE@._V1_.jpg",
+    image: "./images/joanna.jpg",
     actor: 'Stephanie Corneliussen'
   },
   {
     id: 10,
     character: 'Elliot\'s Mother',
-    image: "https://m.media-amazon.com/images/M/MV5BMTg0MzYzNzE0OF5BMl5BanBnXkFtZTgwNjcxMzA3OTE@._V1_.jpg",
+    image: "./images/elliots-mom.jpg",
     actor: 'Vaishnavi Sharma'
   }
 ];
