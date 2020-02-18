976cb73a86189a59a195b9ac49661f0be4e757f5
Brandon C <LIGHTMATTR@Brandons-iMac.local>
2020-02-18 14:44:41 -0800

    attempting a fix for broken images

--- src/robots.js
+++ src/robots.js
@@ -2,61 +2,61 @@ export const robots = [
   {
     id: 1,
     character: 'Elliot Alderson',
-    image: "/images/rami.jpg",
+    image: "./images/rami.jpg",
     actor: 'Rami Malek'
   },
   {
     id: 2,
     character: 'Mr. Robot',
-    image: "/images/christian.jpg",
+    image: "./images/christian.jpg",
     actor: 'Christian Slater'
   },
   {
     id: 3,
     character: 'Darlene',
-    image: "/images/darlene.jpg",
+    image: "./images/darlene.jpg",
     actor: 'Carly Chaikin'
   },
   {
     id: 4,
     character: 'Tyrell Wellick',
-    image: "/images/tyrell.jpg",
+    image: "./images/tyrell.jpg",
     actor: 'Martin Wallström'
   },
   {
     id: 5,
     character: 'Angela Moss',
-    image: "/images/angela.jpg",
+    image: "./images/angela.jpg",
     actor: 'Portia Doubleday'
   },
   {
     id: 6,
     character: 'Phillip Price',
-    image: "/images/phillip.jpg",
+    image: "./images/phillip.jpg",
     actor: 'Michael Cristofer'
   },
   {
     id: 7,
     character: 'Dominique DiPierro',
-    image: "/images/dominique.jpg",
+    image: "./images/dominique.jpg",
     actor: 'Grace Gummer'
   },
   {
     id: 8,
     character: 'Whiterose',
-    image: "/images/whiterose.jpg",
+    image: "./images/whiterose.jpg",
     actor: 'BD Wong'
   },
   {
     id: 9,
     character: 'Joanna Wellick',
-    image: "/images/joanna.jpg",
+    image: "./images/joanna.jpg",
     actor: 'Stephanie Corneliussen'
   },
   {
     id: 10,
     character: 'Elliot\'s Mother',
-    image: "/images/elliots-mom.jpg",
+    image: "./images/elliots-mom.jpg",
     actor: 'Vaishnavi Sharma'
   }
 ];
