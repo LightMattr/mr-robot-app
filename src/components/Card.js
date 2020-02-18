a1946c55746929fa267fb43f2abd068730fb0cd8
Brandon C <LIGHTMATTR@Brandons-iMac.local>
2020-02-18 14:38:49 -0800

    replaced email with actor name

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Components.css';
 
-const Card = ({name, email, id, image}) => {
+const Card = ({name, actor, id, image}) => {
   return (
     <div className="tc bg-near-black dib br3 ma2 grow bw2 shadow-5 Main-card" style={{backgroundImage: `url(${image})`}}>
       <div className="Main-img"></div>
       <div className="Character-info">
         <h2>{name}</h2>
-        <p className="Actor">{email}</p>
+        <p className="Actor">{actor}</p>
       </div>
     </div>
   );
